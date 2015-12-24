fe28aac8e083491e40f9ebaf4ad21f6beb1943ac
Ivan Akulov <gxoptg@gmail.com>
2015-12-24 16:21:48 +0300

    Add TokenDefinitionLoader unit tests

---
+++ tests/TokenDefinitionLoaderTest/TokenDefinitionLoaderTest.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-12-24T15:46:51
+#
+#-------------------------------------------------
+
+QT       += testlib
+
+QT       -= gui
+
+TARGET = tst_tokendefinitionloadertest
+CONFIG   += console c++14
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += tst_tokendefinitionloadertest.cpp
+DEFINES += SRCDIR=\\\"$$PWD/\\\"
---
+++ tests/TokenDefinitionLoaderTest/tst_tokendefinitionloadertest.cpp
@@ -0,0 +1,63 @@
+#include <QString>
+#include <QtTest>
+
+#include "../../src/collections/List.h"
+#include "../../src/structures/tokendefinition.cpp"
+#include "../../src/loaders/tokendefinitionloader.cpp"
+
+Q_DECLARE_METATYPE_TEMPLATE_1ARG(List)
+Q_DECLARE_METATYPE(TokenDefinition)
+
+
+class TokenDefinitionLoaderTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    TokenDefinitionLoaderTest();
+
+private Q_SLOTS:
+    void getDefinitions_data();
+    void getDefinitions();
+};
+
+TokenDefinitionLoaderTest::TokenDefinitionLoaderTest()
+{
+}
+
+void TokenDefinitionLoaderTest::getDefinitions_data()
+{
+    QTest::addColumn<QString>("inputText");
+    QTest::addColumn<List<TokenDefinition>>("parsedTokens");
+
+    QTest::newRow("Valid tokens")
+        << QString("[{'name': 'word', 'pattern': '\\\\w+'}, {'name': 'space', 'pattern': '\\\\s+'}]").replace("'", "\"")
+        << List<TokenDefinition>{
+           TokenDefinition("word", "\\w+"),
+           TokenDefinition("space", "\\s+")
+        };
+
+    QTest::newRow("Invalid file structure")
+        << QString("{'name': 'word', 'pattern': '\\\\w+'}").replace("'", "\"")
+        << List<TokenDefinition>();
+
+    QTest::newRow("Invalid token structure")
+        << QString("[{'name': 'word', 'pattern': '\\\\w+'}, {'name': 'space', 'value': '\\\\s+'}]").replace("'", "\"")
+        << List<TokenDefinition>{
+           TokenDefinition("word", "\\w+")
+        };
+}
+
+void TokenDefinitionLoaderTest::getDefinitions()
+{
+    QFETCH(QString, inputText);
+    QFETCH(List<TokenDefinition>, parsedTokens);
+
+    QTextStream stream(&inputText);
+
+    QCOMPARE(TokenDefinitionLoader(&stream).getDefinitions(), parsedTokens);
+}
+
+QTEST_APPLESS_MAIN(TokenDefinitionLoaderTest)
+
+#include "tst_tokendefinitionloadertest.moc"
