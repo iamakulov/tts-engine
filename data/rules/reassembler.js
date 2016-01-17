c75590a173008ca74edee73775e0304446d73008
Ivan Akulov <gxoptg@gmail.com>
2016-01-17 06:10:48 +0300

    Improve the Russian language configuration

--- data/rules/reassembler.js
+++ data/rules/reassembler.js
@@ -1,31 +1,61 @@
 (function () {
     var TokenName = {
-        NUMBER: 'number',
-        SPACING: 'spacing',
-        PUNCTUATION: 'punctuation'
+        WORD: 'WORD',   // This token name must match the C++ TokenName::WORD value (structures/token.h)
+        NUMBER: 'NUMBER',
+        SPACING: 'SPACING',
+        PUNCTUATION: 'PUNCTUATION'
     };
-    
+
     function createToken(name, data) {
         return {
             name: name,
             data: data
         };
     }
-    
+
+    var isWord = function (token) {
+        return token.name === TokenName.WORD;
+    }
+
     var isNumber = function (token) {
         return token.name === TokenName.NUMBER;
     }
-    
+
     var isSpacing = function (token) {
         return token.name === TokenName.SPACING;
     }
-    
+
+    var isPunctuation = function (symbol) {
+        return function (token) {
+            return token.name === TokenName.PUNCTUATION && token.data === symbol;
+        };
+    }
+
     return [
+        // «красно-синий» → «красно синий»
+        {
+            matchingSequence: [isWord, isPunctuation('-'), isWord],
+            handler: function (tokens) {
+                return [ tokens[0], tokens[2] ];
+            }
+        },
+        // «25 789» → «25789»
         {
             matchingSequence: [isNumber, isSpacing, isNumber],
             handler: function (tokens) {
                 return [ createToken(TokenName.NUMBER, tokens[0].data + tokens[2].data) ];
             }
-        }
+        },
+        // A fractional number («45» + «.» + «789») → a single token
+        {
+            matchingSequence: [
+                isNumber,
+                function(token) { return isPunctuation('.')(token) || isPunctuation(',')(token) },
+                isNumber
+            ],
+            handler: function (tokens) {
+                return [ createToken(TokenName.NUMBER, tokens[0].data + ',' + tokens[2].data) ];
+            }
+        },
     ];
 })();
--- data/rules/transcription.js
+++ data/rules/transcription.js
@@ -1,10 +1,10 @@
 (function () {
     var TokenName = {
-        WORD: 'word',
-        NUMBER: 'number',
-        PUNCTUATION: 'punctuation'
+        WORD: 'WORD',   // This token name must match the C++ TokenName::WORD value (structures/token.h)
+        NUMBER: 'NUMBER',
+        PUNCTUATION: 'PUNCTUATION'
     };
-    
+
     function createToken(name, data) {
         return {
             name: name,
@@ -15,14 +15,42 @@
     var isNumber = function (token) {
         return token.name === TokenName.NUMBER;
     }
-    
+
     var isPunctuation = function (symbol) {
         return function (token) {
             return token.name === TokenName.SPACING && token.data === symbol;
         };
     }
-    
+
     return [
+        {
+            matchingSequence: [isNumber],
+            handler: function (tokens) {
+                var CHARACTER_NAMES = {
+                    '0': 'ноль',
+                    '1': 'один',
+                    '2': 'два',
+                    '3': 'три',
+                    '4': 'четыре',
+                    '5': 'пять',
+                    '6': 'шесть',
+                    '7': 'семь',
+                    '8': 'восемь',
+                    '9': 'девять',
+                    ',': 'запятая'
+                }
+                var token = tokens[0];
+                return token.data.split('').map(function (digit) {
+                    return createToken(TokenName.WORD, CHARACTER_NAMES[digit]);
+                });
+            }
+        },
+        {
+            matchingSequence: [isPunctuation('.')],
+            handler: function (_) {
+                return [ createToken(TokenName.WORD, 'точка') ];
+            }
+        },
         {
             matchingSequence: [isPunctuation(',')],
             handler: function (_) {
