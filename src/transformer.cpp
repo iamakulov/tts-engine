b5ddd1909c5238c6eb72ddc332c86374b5e60db5
Ivan Akulov <gxoptg@gmail.com>
2016-01-17 08:38:46 +0300

    Fix Transformer missing tokens in specific cases

--- src/transformer.cpp
+++ src/transformer.cpp
@@ -9,18 +9,28 @@ Transformer::Transformer(List<TransformationRule> rules)
 List<Token> Transformer::transform(List<Token> tokens) const
 {
     List<Token> transformedTokens = tokens;
-    for (int i = 0; i < transformedTokens.length(); ++i) {
-        m_rules
-            // Process each rule
-            .forEach([&transformedTokens, i, this](TransformationRule rule) {
-                bool ruleNotLongerTokens = rule.matchingSequence().length() <= transformedTokens.length() - i;
-                auto tokensToMatch = transformedTokens.mid(i, rule.matchingSequence().length());
-                if (ruleNotLongerTokens && ruleMatches(rule, tokensToMatch)) {
-                    auto newTokens = rule.handler()(tokensToMatch);
-                    transformedTokens = transformedTokens.mid(0, i) + newTokens + transformedTokens.mid(i + rule.matchingSequence().length());
-                }
-            });
-    }
+    List<Token> previouslyTransformedTokens;
+
+    // When some tokens are replaced with a smaller number of tokens,
+    // some new tokens may appear before the position where transformations are being performed.
+    // Therefore, such tokens will not be transformed.
+    // To ensure that all tokens are always transformed, the transformation is repeated until the list stops changing.
+    do {
+        previouslyTransformedTokens = transformedTokens;
+
+        for (int i = 0; i < transformedTokens.length(); ++i) {
+            m_rules
+                .forEach([&transformedTokens, i, this](TransformationRule rule) {
+                    bool ruleNotLongerTokens = rule.matchingSequence().length() <= transformedTokens.length() - i;
+                    auto tokensToMatch = transformedTokens.mid(i, rule.matchingSequence().length());
+
+                    if (ruleNotLongerTokens && ruleMatches(rule, tokensToMatch)) {
+                        auto newTokens = rule.handler()(tokensToMatch);
+                        transformedTokens = transformedTokens.mid(0, i) + newTokens + transformedTokens.mid(i + rule.matchingSequence().length());
+                    }
+                });
+        }
+    } while (previouslyTransformedTokens != transformedTokens);
 
     return transformedTokens;
 }
